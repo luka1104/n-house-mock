c76a6d3c84c1a40399b3245995e3bcbcb96b0e3b
luka1104 <skyward.luke11@gmail.com>
2023-06-18 10:56:34 +0900

    Update home page text styling and content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,13 @@ const Index: NextPage = () => {
     <>
       <Box w="100%" maxW="400px" h="100%" position="fixed" zIndex="1" bgImage="/images/HomeTop.png" />
       <Box zIndex="100" color="black" position="absolute">
-        <Box pt="475px" pl="33px">
-          <Text>ほげほげほげほげほげ</Text>
-          <Text>ほげほげほげほげほげ</Text>
+        <Box w="293px" pt="475px" pl="33px" color="white" fontFamily="Noto Sans">
+          <Text fontSize="25px" fontWeight={700} lineHeight="1.5">
+            ほげほげほげほげほげ
+          </Text>
+          <Text fontSize="16px" fontWeight={500} lineHeight="1.5">
+            ほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげほげ
+          </Text>
         </Box>
       </Box>
     </>
