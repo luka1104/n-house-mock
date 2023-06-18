fdcfe5db236a0c77efe0553938a4bd95c211fc47
luka1104 <skyward.luke11@gmail.com>
2023-06-18 12:25:43 +0900

    Refactor footer navigation links with dynamic styles

--- src/components/NavFooter.tsx
+++ src/components/NavFooter.tsx
@@ -1,14 +1,22 @@
 import { Box, Center, HStack, Image, Link, Text, VStack } from "@chakra-ui/react"
 import React from "react"
+import { useRouter } from "next/router"
 
 const NavFooter: React.FC = () => {
+  const router = useRouter()
   return (
     <Box position="fixed" zIndex="200" bottom="0" w="100%" maxW="440px" h="74px" bg="transparent">
       <HStack gap="4px" px="4px" h="100%">
         <Link href="/home" w="100%">
           <Center h="100%" w="100%">
             <VStack gap="8px">
-              <Image h="24px" src="/icons/Home.png" />
+              {router.pathname.includes("home") ? (
+                <Center w="64px" h="32px" bg="#00A7C1" borderRadius="16px">
+                  <Image h="24px" src="/icons/Home.png" />
+                </Center>
+              ) : (
+                <Image h="24px" src="/icons/Home.png" />
+              )}
               <Text fontFamily="Roboto" fontSize="12px" lineHeight="1.5" fontWeight="400" color="white">
                 HOME
               </Text>
@@ -18,7 +26,13 @@ const NavFooter: React.FC = () => {
         <Link href="/mypage" w="100%">
           <Center h="100%" w="100%">
             <VStack gap="8px">
-              <Image h="24px" color="black" src="/icons/MyPage.png" />
+              {router.pathname.includes("mypage") ? (
+                <Center w="64px" h="32px" bg="#00A7C1" borderRadius="16px">
+                  <Image h="24px" color="black" src="/icons/MyPage.png" />
+                </Center>
+              ) : (
+                <Image h="24px" color="black" src="/icons/MyPage.png" />
+              )}
               <Text fontFamily="Roboto" fontSize="12px" lineHeight="1.5" fontWeight="400" color="white">
                 MY PAGE
               </Text>
@@ -28,7 +42,13 @@ const NavFooter: React.FC = () => {
         <Link href="/setting" w="100%">
           <Center h="100%" w="100%">
             <VStack gap="8px">
-              <Image w="24px" src="/icons/Setting.png" />
+              {router.pathname.includes("setting") ? (
+                <Center w="64px" h="32px" bg="#00A7C1" borderRadius="16px">
+                  <Image w="24px" src="/icons/Setting.png" />
+                </Center>
+              ) : (
+                <Image w="24px" src="/icons/Setting.png" />
+              )}
               <Text fontFamily="Roboto" fontSize="12px" lineHeight="1.5" fontWeight="400" color="white">
                 SETTING
               </Text>
