8aabff5721ac92125b55592053c1958f9b27fd43
luka1104 <skyward.luke11@gmail.com>
2023-06-18 10:47:06 +0900

    Add global styles to theme

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  styles: {
+    global: {
+      p: {
+        color: "white",
+        fontSize: "16px",
+        fontWeight: "500",
+        fontFamily: "Noto Sans",
+        lineHeight: "24px",
+      },
+    },
+  },
+}
