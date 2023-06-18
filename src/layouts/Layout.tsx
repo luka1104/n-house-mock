96c30f336bb91a09ef471e70fb67a9ef933b1c1e
luka1104 <skyward.luke11@gmail.com>
2023-06-18 12:31:49 +0900

    Hide footer on homepage

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,20 +1,22 @@
 import NavFooter from "@/components/NavFooter"
-import Footer from "@/components/NavFooter"
 import { Box, Container } from "@chakra-ui/react"
 import { ReactElement, memo, useEffect, useState } from "react"
+import { useRouter } from "next/router"
 
 type LayoutProps = Required<{
   readonly children: ReactElement
 }>
 
 const Layout = memo(({ children }: LayoutProps) => {
+  const router = useRouter()
+  const hideFooter = router.pathname === "/"
   return (
     <>
       <Container maxW="440px" h="100%" minH="100vh" p={0} bgColor="#000000">
         <Box w="100%" h="100%">
           {children}
         </Box>
-        <NavFooter />
+        {hideFooter ? null : <NavFooter />}
       </Container>
     </>
   )
