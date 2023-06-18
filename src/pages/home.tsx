a3e3521e078b74f363426e7a244f0490452c1d71
luka1104 <skyward.luke11@gmail.com>
2023-06-18 11:40:12 +0900

    Refactor home page layout with Flex

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,16 @@
 import PropertyCard from "@/components/PropertyCard"
 import { properties } from "@/data/mockdata"
-import { Box } from "@chakra-ui/react"
+import { Box, Flex } from "@chakra-ui/react"
 import { NextPage } from "next"
 import React from "react"
 const Home: NextPage = () => {
   return (
     <>
-      <Box w="100%">
+      <Flex flexDirection="column" gap="32px" w="100%">
         {properties.map((property) => (
           <PropertyCard property={property} />
         ))}
-      </Box>
+      </Flex>
     </>
   )
 }
