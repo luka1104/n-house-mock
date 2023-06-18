c8aea944a71900e9536b7a7854a628064d3c5791
luka1104 <skyward.luke11@gmail.com>
2023-06-18 10:45:03 +0900

    Add top image and headline text to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react"
 import { NextPage } from "next"
 import React from "react"
 
 const Index: NextPage = () => {
-  return (<Box bgImage={}>
-    Hello World!
-  </Box>)
-
+  return (
+    <>
+      <Box w="100%" maxW="400px" h="100%" position="fixed" zIndex="1" bgImage="/images/HomeTop.png" />
+      <Box zIndex="100" color="black" position="absolute">
+        <Box pt="475px" pl="33px" color="white" fontFamily="Noto Sans">
+          <Text>ほげほげほげほげほげ</Text>
+          <Text>ほげほげほげほげほげ</Text>
+        </Box>
+      </Box>
+    </>
+  )
+}
 export default Index
