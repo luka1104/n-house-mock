139ec16a241cdd7f46b88c0d63e7b8be3babe551
luka1104 <skyward.luke11@gmail.com>
2023-06-18 10:49:32 +0900

    Remove unused theme in _app.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import Layout from "@/layouts/Layout"
-import { styles } from "@/themes"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={styles}>
+    <ChakraProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
