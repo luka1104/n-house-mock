01bb9934476045a4c96db0fe3e7aa08eff608d66
luka1104 <skyward.luke11@gmail.com>
2023-06-18 10:48:52 +0900

    Refactor global theme styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import Layout from "@/layouts/Layout"
-import { theme } from "@/themes"
+import { styles } from "@/themes"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider theme={styles}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Index: NextPage = () => {
     <>
       <Box w="100%" maxW="400px" h="100%" position="fixed" zIndex="1" bgImage="/images/HomeTop.png" />
       <Box zIndex="100" color="black" position="absolute">
-        <Box pt="475px" pl="33px" color="white" fontFamily="Noto Sans">
+        <Box pt="475px" pl="33px">
           <Text>ほげほげほげほげほげ</Text>
           <Text>ほげほげほげほげほげ</Text>
         </Box>
