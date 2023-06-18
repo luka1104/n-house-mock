2316e2b714d4494917579ce529b11cd03695534d
luka1104 <skyward.luke11@gmail.com>
2023-06-18 11:27:50 +0900

    Add PropertyCard component to homepage

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,19 +1,18 @@
-import { Box, Link } from "@chakra-ui/react"
+import { Box, Link, Text } from "@chakra-ui/react"
 import React from "react"
 
 type Props = {
-  image: string
-  title: string
-  description: string
-  label: string
-  href: string
+  property: any
 }
 
-const PropertyCard: React.FC<Props> = ({ image, title, description, label, href }) => {
+const PropertyCard: React.FC<Props> = ({ property }) => {
   return (
     <>
-      <Link href={href}>
-        <Box w="100%" maxW="440px" borderRadius="16px" bgImage={image}></Box>
+      <Link w="100%" href={property.href}>
+        <Box w="100%" h="560px" bgSize="cover" maxW="440px" borderRadius="16px" bgImage={property.image}>
+          <Text>N’HOUSE</Text>
+          <Text>{property.title}</Text>
+        </Box>
       </Link>
     </>
   )
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,18 @@
+import PropertyCard from "@/components/PropertyCard"
+import { properties } from "@/data/mockdata"
+import { Box } from "@chakra-ui/react"
 import { NextPage } from "next"
 import React from "react"
-
 const Home: NextPage = () => {
-  return <div>Home</div>
+  return (
+    <>
+      <Box w="100%">
+        {properties.map((property) => (
+          <PropertyCard property={property} />
+        ))}
+      </Box>
+    </>
+  )
 }
 
 export default Home
