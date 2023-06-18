09448ef7ac05868d8201724989bded7376d92ebc
luka1104 <skyward.luke11@gmail.com>
2023-06-18 10:47:33 +0900

    Add theme to ChakraProvider in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import Layout from "@/layouts/Layout"
+import { theme } from "@/themes"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
