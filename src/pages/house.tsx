e9d386fe6f4f70c111f480efbbe058a4ced9ba2a
luka1104 <skyward.luke11@gmail.com>
2023-06-19 23:46:33 +0900

    Add swiper to house page for property slideshow

--- src/pages/house.tsx
+++ src/pages/house.tsx
@@ -3,12 +3,21 @@ import { properties } from "@/data/mockdata"
 import { Box, Text } from "@chakra-ui/react"
 import { NextPage } from "next"
 import React from "react"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Pagination } from "swiper"
+import "swiper/css"
+import "swiper/css/pagination"
 
 const House: NextPage = () => {
-  const property = properties[0]
   return (
     <>
-      <PropertyReservationCard property={property} />
+      <Swiper modules={[Pagination]} spaceBetween={50} slidesPerView={1} pagination={{ clickable: true }}>
+        {properties.map((property) => (
+          <SwiperSlide>
+            <PropertyReservationCard property={property} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   )
 }
