75003d4101e2721ddf4d87d379023d25291cc61c
luka1104 <skyward.luke11@gmail.com>
2023-06-19 20:56:11 +0900

    Add logo and account icon to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,27 @@
-import { Box, HStack, Image } from "@chakra-ui/react"
+import { Box, HStack, Image, Link, Text } from "@chakra-ui/react"
 import React from "react"
 
 const Header: React.FC = () => {
   return (
-    <HStack position="fixed" zIndex="200" top="0" w="100%" maxW="440px" h="64px" bg="white">
-      <Box>
-        <Image src="/icons/Setting.png" />
-      </Box>
+    <HStack
+      position="fixed"
+      zIndex="200"
+      top="0"
+      w="100%"
+      maxW="440px"
+      h="64px"
+      bg="white"
+      justifyContent="space-between"
+    >
+      <Link href="/home" w="100%" textDecoration="none !important">
+        <Image py="20px" pl="16px" src="/icons/Setting.png" />
+      </Link>
+      <Link href="/home" w="100%" textDecoration="none !important">
+        <Text>N'HOUSE</Text>
+      </Link>
+      <Link href="/home" w="100%" textDecoration="none !important">
+        <Image py="20px" pr="12px" src="/icons/Account.png" />
+      </Link>
     </HStack>
   )
 }
